a09781ff49fe7e8983b08b5c81eeef3c257e3459
yleedon <yleedon@gmail.com>
2018-10-31 19:22:29 +0200

    first change

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package View;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
