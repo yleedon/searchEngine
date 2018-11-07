9e0c06a6ef8db2c5a9822644b3d20dd617e5f94e
Dan <dan.avraham23@gmail.com>
2018-11-07 18:05:34 +0200

    ReadFile - has parser and parsing during reading Indexer.parse() is tester now

--- src/Model/Indexer.java
+++ src/Model/Indexer.java
@@ -17,33 +17,14 @@ public class Indexer {
     }
     public void parse(){
         try {
-//            ClassLoader classLoader = getClass().getClassLoader();
-            FileReader fileReader = new FileReader(new File("C:\\Users\\Yaniv\\Desktop\\searchproject\\searchEngine\\out\\production\\SearchEngine\\documentIdx.txt"));
-            BufferedReader reader = new BufferedReader(fileReader);
-            String line = reader.readLine();
-
-            long startTime = System.nanoTime();
-
-            int numberOfDocs = 0;
-            while (reader.ready()){
-
-                line = line.split(",")[0];
-
-                parser.setTxt( rf.getDocument(line).getTxt());
-                System.out.println("********************************doc name is: " + line+ "*************************************************");
-                parser.parse();
-                parser.printIndex();
-                numberOfDocs++;
-                line = reader.readLine();
-            }
-            long endTime = System.nanoTime();
-
-            long duration = (endTime - startTime);
-            System.out.println("process finished successfully\ntotal time: "+ duration/1000000 + "ms\nnum of docs: "+ numberOfDocs);
+            long start = System.nanoTime();
+            rf.readDirectory();
+            long end = System.nanoTime();
+            double time = end-start;
+            System.out.println(time/1000000000);
         }
         catch(Exception e){
             System.out.println("something went wrong!!!");
-            return;
         }
     }
 }
