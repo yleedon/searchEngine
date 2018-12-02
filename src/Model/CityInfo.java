2161eba3a150779ba2aa5b89230570caa3ecec09
Dan <dan.avraham23@gmail.com>
2018-12-02 16:43:12 +0200

    CityInfo ready Class CityInfo complete + yaniv's notGoogle image is here now named "prev.jpg" in resources

---
+++ src/Model/CityInfo.java
@@ -0,0 +1,54 @@
+package Model;
+
+import eu.fayder.restcountries.v1.domain.Country;
+import eu.fayder.restcountries.v1.rest.CountryService;
+
+import java.util.List;
+
+public class CityInfo {
+    String city, country, population, currency;
+    Country countryData;
+    CountryService service;
+
+    public CityInfo(String city) {
+        this.city = city;
+        setCountryData();
+        population = null;
+        currency = null;
+        service = eu.fayder.restcountries.v1.rest.CountryService.getInstance();
+    }
+
+    private void setCountryData() {
+        List<Country> countries = service.getByCapital(city);
+        if (countries.size() > 0)
+            countryData = countries.get(0);
+        else countryData = null;
+    }
+
+    public String getCountry() {
+        if (country == null) {
+            if (countryData == null) country = "";
+            else country = countryData.getName();
+        }
+        return country;
+    }
+
+    public String getPopulation() {
+        if (population == null) {
+            if (countryData == null) population = "";
+            else population = "" + countryData.getPopulation();
+        }
+        return population;
+    }
+
+    public String getCurrency() {
+        if (currency == null) {
+            if (countryData == null) currency = "";
+            else {
+                List<String> currencies = countryData.getCurrencies();
+                currency = currencies.get(0);
+            }
+        }
+        return currency;
+    }
+}
