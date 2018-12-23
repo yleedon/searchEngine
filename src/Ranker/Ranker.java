731ac000e4056550f60788790a13df83a0c3c6a5
yleedon <yleedon@gmail.com>
2018-12-23 13:26:40 +0200

    query case sensetivity disabled + java doc (Ranker and Searcher)

--- src/Ranker/Ranker.java
+++ src/Ranker/Ranker.java
@@ -2,8 +2,6 @@ package Ranker;
 
 import Indexer.DicEntry;
 import javafx.util.Pair;
-import processing.MyDocument;
-
 import java.io.*;
 import java.util.*;
 
@@ -17,36 +15,72 @@ public class Ranker implements IRanker {
     private double avrageTermCount;
     private PriorityQueue<TermDocData> docDataList;
 
+    /**
+     * constructor, retrieves all the necessary data for ranking the documents.
+     * @param dataPath - the path where all the disc data is at
+     * @param quaryMap - the terms in the quarry with the frequency
+     * @param filteredDocs - the filtered docs by city
+     * @param averageTermCount - the total average term count in the dataBase
+     * @throws Exception - from private functions (I/O)
+     */
     public Ranker(String dataPath, Map<String, Pair<Integer, Integer>> quaryMap, TreeSet<String> filteredDocs, double averageTermCount) throws Exception {
         this.outPut = dataPath;
         this.quaryMap = quaryMap;
         this.filteredDocs = filteredDocs;
         this.avrageTermCount = averageTermCount;
+        this.docDataList = new PriorityQueue<>();
+
         if(filteredDocs == null || filteredDocs.size() ==0 )
             filterOn = false;
         else filterOn = true;
-
-        docDataList = new PriorityQueue<>();
 
         loadDictionary();
         getReleventDocs();
     }
 
+    /**
+     * goes over each term in the query, gets its posting list line and creates adds the data to the "docDataList"
+     * @throws Exception
+     */
     private void getReleventDocs() throws Exception {
         for(String term:quaryMap.keySet()){
+            ////////////////////////////////////
+            if(!dictianary.containsKey(term))
+                term = capitalSensitivetyFix(term);
+            ////////////////////////////////////
+
+
             if(dictianary.containsKey(term)){
                 String posting = getPosting(dictianary.get(term).getId());
                 String[] docs = posting.split(":")[1].split("~");
 
                 createTermDocData(docs,term);
-
-
             }
         }
     }
 
+    /**
+     * this function makes the query not case sensitive (for upper or lower case terms)
+     * checks if the term exists in a capital state, if so return that state
+     * @param term - the term that is to become not case sensitive
+     * @return - the term in a capital state the exists in the dittionary, or the term as is if does not exist in dictionary
+     */
+    private String capitalSensitivetyFix(String term) {
+        if(dictianary.containsKey(term.toUpperCase()))
+            return term.toUpperCase();
+        if (dictianary.containsKey(term.toLowerCase()))
+            return term.toLowerCase();
+        return term;
+    }
+
+    /**
+     * givven a posting list line, this function splits the line by docs (and fixes the gaps) and retrieves the information
+     * this function takes into account the "city Filter" (will process only cities selected if selected)
+     * @param docs - the list of doxs+info from the posting list
+     * @param term - the term that the docs belong to
+     * @throws Exception - "addTermDocs()" exceptions
+     */
     private void createTermDocData(String[] docs,String term) throws Exception {
-        ArrayList<MyDocument> ans = new ArrayList<>();
         int curretnDoc = 0;
         int gap ;
         for(String docInfo:docs){
@@ -60,12 +94,15 @@ public class Ranker implements IRanker {
                 continue;
 
             addTermDocData(docInfoSplit,term);
-
         }
-
     }
-    //check posting list for correctness!!!
 
+    /**
+     * given  the information from the posting list, creates a "TermDocData" object and adds it to the priority queue.
+     * @param docInfo - the information of a certain document in the posting list (without gaps)
+     * @param term - the term that the document belongs to
+     * @throws Exception - thrown from "getDocTotalTermAmount()"
+     */
     private void addTermDocData(String[] docInfo, String term) throws Exception {
         boolean isInTitle = false;
         if(docInfo[2].contains("@")){
@@ -79,6 +116,12 @@ public class Ranker implements IRanker {
         docDataList.add(termDocData);
     }
 
+    /**
+     * retrieves the totalTermCount in the givven document
+     * @param doc - the documrnt ID
+     * @return - the total amount of terms in the document
+     * @throws Exception - doxIdx,txt psth not found
+     */
     private int getDocTotalTermAmount(int doc) throws Exception {
         try {
             File docIdx = new File(outPut + "docIdx.txt");
@@ -97,10 +140,14 @@ public class Ranker implements IRanker {
         catch (Exception e){
             throw new Exception("docIdx.txt not found\nRanker_GetTotalTermAmount (docNumber: "+doc+" )");
         }
-
-
     }
 
+    /**
+     * retrieves the terms posting list
+     * @param id - term ID
+     * @return - the posting line of the term
+     * @throws Exception - posting list path not found
+     */
     private String getPosting(int id) throws Exception {
         File postingList = new File(outPut+"postingList.txt");
         try{
@@ -116,17 +163,18 @@ public class Ranker implements IRanker {
         }
     }
 
-
+    /**
+     * loads the dictionary from the disc
+     * @throws Exception - dictionary path not found
+     */
     public void loadDictionary() throws Exception {
 
         File file = new File(outPut+"dictionary.txt");
         if (!file.exists()) {
-           throw new Exception("dictionary not found:\n"+outPut+"dictionary.txt");
+            throw new Exception("dictionary not found:\n"+outPut+"dictionary.txt");
         }
-
         dictianary = new TreeMap<>();
         try {
-
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
             String[] line;
@@ -135,7 +183,6 @@ public class Ranker implements IRanker {
                 if (st.equals("")) {
                     continue;
                 }
-
                 line = st.split(":");
                 data = line[1].split(",");
                 DicEntry entry = new DicEntry(Integer.valueOf(data[0]));
@@ -143,12 +190,9 @@ public class Ranker implements IRanker {
                 entry.totalTermFrequency = Integer.valueOf(data[2]);
                 dictianary.put(line[0], entry);
             }
-
-
             br.close();
-
         } catch (Exception e) {
-           throw new Exception("load dictionary error");
+            throw new Exception("load dictionary error");
         }
     }
 }
